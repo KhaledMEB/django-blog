41d26cdb6a5143f0651b0f4250ce57b4fcf9b51f
shaddow99 <gk_mebhah@esi.dz>
2020-06-07 14:47:10 +0100

    Add Login & Logout

--- users/views.py
+++ users/views.py
@@ -7,9 +7,9 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            #username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account had been created! your are able to log in')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
