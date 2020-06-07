2b68bb09425b7b19642116196a7cfec18ac1059f
shaddow99 <gk_mebhah@esi.dz>
2020-06-07 15:48:31 +0100

    Finish Authentification system

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', users_views.register, name='register'), # use the view directly from users
+    path('profile/', users_views.profile, name='profile'),
     path('', include('blog.urls')) # use the url pattern inside blog
 ]
