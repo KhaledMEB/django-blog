b5c15f73475436e52832d6215bc6dcd4e25f9060
shaddow99 <gk_mebhah@esi.dz>
2020-06-09 19:13:42 +0100

    Add LoginRequiredMixin to PostCreateView

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import (
     ListView, DetailView, CreateView)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # function based view
 # def home(request):
@@ -20,7 +21,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
